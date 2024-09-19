741e9c21da2bc873712265475e2ad142c2c5c2c0
Turker Sezer <80282401+tsezer-rxphoto@users.noreply.github.com>
2024-09-19 03:48:22 +0300

    Update baseapi.py

--- pyfcm/baseapi.py
+++ pyfcm/baseapi.py
@@ -87,7 +87,7 @@ class BaseAPI(object):
             retries = Retry(
                 backoff_factor=1,
                 status_forcelist=[502, 503],
-                allowed_methods=(Retry.DEFAULT_METHOD_WHITELIST | frozenset(["POST"])),
+                method_whitelist=(Retry.DEFAULT_METHOD_WHITELIST | frozenset(["POST"])),
             )
             adapter = self.custom_adapter or HTTPAdapter(max_retries=retries)
             self.thread_local.requests_session = requests.Session()
